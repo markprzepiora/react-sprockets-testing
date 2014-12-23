2a00686cd98a53ed468df75d72443b4a6544b9a8
Mark Przepiora <mark.przepiora@gmail.com>
2014-12-23 10:50:19 -0700

    Add a working compile script

---
+++ src/app.js
@@ -0,0 +1,4 @@
+//= require_tree .
+
+console.log('this is app.js');
+console.log('another line');
---
+++ src/bar.jsx
@@ -0,0 +1,5 @@
+//= require ./foo
+
+console.log('this is bar.js');
+
+var domthingie = <h1>Hi!</h1>;
